30e16c1dfdd804eb212a9aba0eac890cb2b6aa88
Yusuf <kurakiorg@gmail.com>
2023-06-21 21:50:49 +0300

    Todo Add

--- todo/urls.py
+++ todo/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from .views import (
     # FBV:
     todo_list,
+    todo_add,
 )
 
 # after '/':
 urlpatterns = [
-    path('list/', todo_list),
+    path('list/', todo_list, name='list'),
+    path('add/', todo_add, name='add'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Todo
-
+from .forms import TodoForm
 
 def todo_list(request):
     todos = Todo.objects.all()
@@ -8,3 +8,23 @@ def todo_list(request):
         'todos': todos
     }
     return render(request, 'list.html', context)
+
+from django.contrib import messages
+from django.shortcuts import redirect
+
+def todo_add(request):
+    # if request.POST:
+    #     form = TodoForm(request.post)
+    #     form.save()
+    # else :
+    #     form = TodoForm()
+
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Added.')
+        return redirect('list')
+    context = {
+        'form': form
+    }
+    return render(request, 'add.html', context)
