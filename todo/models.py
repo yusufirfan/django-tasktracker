799819d706800af1163d6c39bb39e7bc14e24c34
Yusuf <kurakiorg@gmail.com>
2023-06-21 20:27:42 +0300

    Models, forms and urls

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [
+    ('C', 'Completed'),
+    ('P', 'Pending'),
+]
+
+priority_choices = [
+    (1, 'High'),
+    (2, 'Medium'),
+    (3, 'Low'),
+]
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=status_choices, default='P')
+    priority = models.SmallIntegerField(choices=priority_choices, default=2)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
