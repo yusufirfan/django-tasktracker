5630dba601109f7c3703156948fb4d9b764d3557
Yusuf <kurakiorg@gmail.com>
2023-06-21 22:23:51 +0300

    Update

--- todo/urls.py
+++ todo/urls.py
@@ -4,10 +4,14 @@ from .views import (
     # FBV:
     todo_list,
     todo_add,
+    todo_update,
+    todo_delete,
 )
 
 # after '/':
 urlpatterns = [
     path('list/', todo_list, name='list'),
     path('add/', todo_add, name='add'),
+    path('update/<int:pk>', todo_update, name='todo_update'),
+    path('delete/<int:pk>', todo_delete, name='todo_delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -28,3 +28,23 @@ def todo_add(request):
         'form': form
     }
     return render(request, 'add.html', context)
+
+def todo_update(request, pk):
+    todo = Todo.objects.get(id=pk)
+    form = TodoForm(instance=todo)
+    if request.method== "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Güncellendi.')
+            return redirect("index")
+    return render(request, 'update.html', {
+        'form': form,
+        'todo': todo
+    })
+
+def todo_delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    messages.success(request, 'Silindi.')
+    return redirect("index")
